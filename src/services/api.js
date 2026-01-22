ca06da94cf0005e2529b8284d031964bb81eb70d
Falouhiayoub <falouhiayoub9@gmail.com>
2026-01-22 11:18:02 +0100

    add the crud for the admin control panel

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'https://69528ac13b3c518fca12fccd.mockapi.io/api/v1/',
+    headers: {
+        'Content-type' : 'application/json',
+    }
+})
+
+export const getProducts = () => api.get('/products')
+export const getproduct = (id) => api.get(`/products/${id}`)
+export const createProduct = (product) => api.post('/products', product)
+export const updateProduct = (id, product) => api.put(`/products/${id}`, product)
+export const deleteProduct = (id) => api.delete(`/products/${id}`)
+
+
+
+export default api
