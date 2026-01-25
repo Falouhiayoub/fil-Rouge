0401959798aff818e86906b6b90f50b1f55e53c3
Falouhiayoub <falouhiayoub9@gmail.com>
2026-01-25 01:56:42 +0100

    adding the product and the cart reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
+import createReducer from "./slices/cartSlice";
+import productReducer from './slices/productSlice';
+
 
 export const store = configureStore({
     reducer: {
+        products: productReducer,
+        cart: createReducer,
         auth: authReducer,
     },
 });
